eae163ae640841afce16b8b3df5a3f620fbb9414
hp-PC\hp <Mahdi.heravi@gmail.com>
2016-12-04 13:08:23 +0330

    second Test for loop on button and linearlayouts

--- app/src/main/java/com/example/hp/exersise7_marpele/MainActivity.java
+++ app/src/main/java/com/example/hp/exersise7_marpele/MainActivity.java
@@ -10,34 +10,30 @@ import android.widget.LinearLayout;
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout[] LL;
+    Button[] btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LL = new LinearLayout[10];
-        Button btn1 = new Button(this);
-        btn1.setText("btn1");
-        Button btn2 = new Button(this);
-        btn2.setText("btn2");
-        Button btn3 = new Button(this);
-        btn3.setText("ggggggg");
+        LL = new LinearLayout[11];
+        btn = new Button[101];
+
 
         LinearLayout parentLayout = (LinearLayout) findViewById(R.id.LL);
-
-        LL[0] = new LinearLayout(this);
-        LL[0].setOrientation(LinearLayout.HORIZONTAL);
-        LL[0].setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT));
-        parentLayout.addView(LL[0]);
-        LL[0].addView(btn1);
-        LL[0].addView(btn2);
-
-        LL[1] = new LinearLayout(this);
-        LL[1].setOrientation(LinearLayout.HORIZONTAL);
-        LL[1].setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT));
-        parentLayout.addView(LL[1]);
-        LL[1].addView(btn3);
-
+        int k;
+        for (int i = 1; i <= 10; i++) {
+            LL[i] = new LinearLayout(this);
+            LL[i].setOrientation(LinearLayout.HORIZONTAL);
+            LL[i].setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT));
+            parentLayout.addView(LL[i]);
+            for (int j = 1; j <= 10; j++) {
+                k = i * j;
+                btn[k] = new Button(this);
+                btn[k].setText("btn");
+                LL[i].addView(btn[k]);
+            }
+        }
 
 
     }
